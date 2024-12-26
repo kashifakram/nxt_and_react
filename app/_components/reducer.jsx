b6d1a29798b34a4c9e1ba9a5d597f904ca477cf0
Kashif Akram <kashif.akram@bom.gov.au>
2024-12-26 19:20:53 +1100

    renamings

--- app/_components/reducer.jsx
+++ app/_components/reducer.jsx
@@ -1,6 +1,6 @@
 import { useReducer } from 'react';
-import AddTask from './AddTask.js';
-import TaskList from './TaskList.js';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
 
 export default function TaskApp() {
   const [tasks, dispatch] = useReducer(
